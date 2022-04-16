421ed54506bebd96d7e17e5dc5a557bf3362183a
Rudy Hidayat <m_rudy@mhs.unsyiah.ac.id>
2022-04-16 06:32:34 -0700

    Update Favicon

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,7 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/style.css">
-    <title>Login</title>
+    <link rel="shorcut icon" href="/assets/icon/favicon.ico">
+    <title>MengHeal</title>
 </head>
 <body>
     <div class="login-box">
