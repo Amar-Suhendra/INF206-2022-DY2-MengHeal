f622f969ee50ee4e42c660a85e999642eeaa9e26
Rudy Hidayat <m_rudy@mhs.unsyiah.ac.id>
2022-04-17 03:25:55 -0700

    Perbaikan Landing

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,31 +1,23 @@
-@extends('layout/main')
+@extends('layout.main')
 
 @section('container')
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/style.css">
-    <title>Landing</title>
-</head>
-<body>
-    <main>
-        <div class="container">
-            <div class="top-wrapper">
-                <div class="row">
-                    <div class="deskripsi-1">
-                        <h2>Selamat Datang!</h2>
-                        <p>Halo, web ini dibuat untuk memenuhi tugas projek tim Rekayasa Perangkat Lunak.</p>
-                        <p>Web ini ditujukan untuk memberikan informasi mengenai kesehatan mental seseorang, terkhusus
-                            pada remaja. Dewasa ini banyak kita temukan, remaja yang bunuh diri.
-                        </p>
-                    </div>
-                    <div class="img-section-1">
-                        <img src="../assets/img/training.jpg" class="img-1">
-                    </div>
-                </div>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <div class="deskripsi-1">
+                <h2>Selamat Datang!</h2>
+                <p>Halo, web ini dibuat untuk memenuhi tugas projek tim Rekayasa Perangkat Lunak.</p>
+                <p>Web ini ditujukan untuk memberikan informasi mengenai kesehatan mental seseorang, terkhusus
+                    pada remaja. Dewasa ini banyak kita temukan, remaja yang bunuh diri.
+                </p>
             </div>
         </div>
-    </main>
-</body>
+        <div class="col">
+            <img src="../assets/img/training.jpg">
+        </div>
+    </div>
+</div>
+
+
 @endsection
