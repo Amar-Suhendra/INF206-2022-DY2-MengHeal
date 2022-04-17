a84d5d18a65a523ba76e73f6ba5347a8df88ca1b
Rudy Hidayat <m_rudy@mhs.unsyiah.ac.id>
2022-04-17 02:53:10 -0700

    Perubahan Isi landing

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -15,9 +15,9 @@
                 <div class="row">
                     <div class="deskripsi-1">
                         <h2>Selamat Datang!</h2>
-                        <p>Halo, web ini dibuat untuk memenuhi tugas projek tim program Digital Talent Scholarship Progate.</p>
-                        <p>Web ini ditujukan untuk memberikan informasi mengenai kursus, seminar dan workshop yang 
-                            diselenggarakan oleh beberapa universitas.
+                        <p>Halo, web ini dibuat untuk memenuhi tugas projek tim Rekayasa Perangkat Lunak.</p>
+                        <p>Web ini ditujukan untuk memberikan informasi mengenai kesehatan mental seseorang, terkhusus
+                            pada remaja. Dewasa ini banyak kita temukan, remaja yang bunuh diri.
                         </p>
                     </div>
                     <div class="img-section-1">
