16c3e884c09dba17764eeca4ffba4f74215844c0
ikhsan <m_ikhsan20@mhs.unsyiah.ac.id>
2022-04-21 16:51:06 +0700

    add colom in register tabel

--- database/migrations/2022_04_21_091408_create_registers_table.php
+++ database/migrations/2022_04_21_091408_create_registers_table.php
@@ -15,6 +15,12 @@ return new class extends Migration
     {
         Schema::create('registers', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('username')->unique();
+            $table->string('email')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
