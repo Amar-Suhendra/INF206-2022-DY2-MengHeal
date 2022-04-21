fb81576947651e4149c99f6cb8ed59a7a9246e39
Muhammad Rizky Hidayah Akbar <mhidayahakbar@gmail.com>
2022-04-21 15:40:36 +0700

    tambahan konfirmasi antrian

--- resources/views/konfirmasi.blade.php
+++ resources/views/konfirmasi.blade.php
@@ -1,5 +1,24 @@
 @extends('layout/main')
 
 @section('container')
-    <h1>Konfirmasi antrian</h1>
+<div class="d-flex justify-content-center p-5">
+        <div class="card text-center " style="width: 18rem; height:16rem; border-radius: 10%">
+            <div class="card-header bg-transparent">
+                <div class="card-title">
+                    <h3>Nomor antrian berhasil dikonfirmasi</h3>
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="card-text">
+                    <br>
+                    <h1><b></b></h1>
+                </div>
+            </div>
+            <div class="card-footer bg-transparent">
+                <a href="#" class="btn btn-primary" style="width:14rem;">
+                    OK
+                </a>
+            </div>
+        </div>
+    </div>
 @endsection
