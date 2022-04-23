dffc86d07ed996209674000315ad11d3ea6577e6
ikhsan <m_ikhsan20@mhs.unsyiah.ac.id>
2022-04-24 00:45:41 +0700

    change name user

--- resources/views/admin/layout/main.blade.php
+++ resources/views/admin/layout/main.blade.php
@@ -91,7 +91,7 @@
                                     alt="User Image">
                             </div>
                             <div class="info">
-                                <a href="#" class="d-block">Alexander Pierce</a>
+                                <a href="#" class="d-block">Admin</a>
                             </div>
                         </div>
 
