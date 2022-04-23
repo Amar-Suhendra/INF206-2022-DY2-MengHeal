6aa4c59163617ec31e197240514f180125c5a772
ikhsan <m_ikhsan20@mhs.unsyiah.ac.id>
2022-04-23 22:18:49 +0700

    merapikan kode

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -60,15 +60,26 @@
                         <li><a href="#">
                                 <ion-icon name="log-out-outline"></ion-icon>
                             </a></li>
-                    @endauth
-                    @guest
+                        <div class="dropdown">
+                            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                Dropdown
+                            </button>
+                            <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                                <li><button class="dropdown-item" type="button">Action</button></li>
+                                <li><button class="dropdown-item" type="button">Another action</button></li>
+                                <li><button class="dropdown-item" type="button">Something else here</button></li>
+                            </ul>
+                        </div>
+                    @else
                         <li class="nav-item">
                             <a class="nav-link" href="/login">Login |</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="/register">Daftar</a>
                         </li>
-                    @endguest
+                    @endauth
+
 
 
                 </ul>
