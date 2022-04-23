7a96c2041da4e03c534735bb1a8e7ce233d97201
ikhsan <m_ikhsan20@mhs.unsyiah.ac.id>
2022-04-23 23:27:43 +0700

    change positon user login

--- resources/views/admin/layout/main.blade.php
+++ resources/views/admin/layout/main.blade.php
@@ -179,14 +179,7 @@
             <!-- Sidebar -->
             <div class="sidebar">
                 <!-- Sidebar user panel (optional) -->
-                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                    <div class="image">
-                        <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-                    </div>
-                    <div class="info">
-                        <a href="#" class="d-block">Alexander Pierce</a>
-                    </div>
-                </div>
+
 
                 <!-- SidebarSearch Form -->
                 <div class="form-inline">
@@ -245,6 +238,16 @@
                                 </p>
                             </a>
                         </li>
+
+                        <!-- user login -->
+                        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                            <div class="image">
+                                <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                            </div>
+                            <div class="info">
+                                <a href="#" class="d-block">Alexander Pierce</a>
+                            </div>
+                        </div>
 
                         <!-- /.sidebar-menu -->
             </div>
