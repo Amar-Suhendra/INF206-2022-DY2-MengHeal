5ff0b75b784a329f8a491e80535ca0191dda4926
ikhsan <m_ikhsan20@mhs.unsyiah.ac.id>
2022-04-24 00:52:21 +0700

    add logout button at admin page

--- resources/views/admin/layout/main.blade.php
+++ resources/views/admin/layout/main.blade.php
@@ -42,7 +42,12 @@
             <!-- Right navbar links -->
             <ul class="navbar-nav ml-auto">
 
-
+                <li class="nav-item d-none d-sm-inline-block">
+                    <a href="" class="nav-link">
+                        <i class="fas fa-sign-out-alt"></i>
+                        Logout
+                    </a>
+                </li>
 
 
             </ul>
@@ -139,8 +144,7 @@
 
         <!-- Main Footer -->
         <footer class="main-footer">
-            <strong>Copyright &copy; 2014-2021 <a href="#">MenHeal</a>.</strong>
-            All rights reserved.
+            <strong>Copyright &copy; 2022 <a href="#">MenHeal</a>.</strong>
             <div class="float-right d-none d-sm-inline-block">
                 <b>Version</b> 1.0.0
             </div>
