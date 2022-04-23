e2ad95cde9fe54c3da1c063a84d89d2a06b30817
ikhsan <m_ikhsan20@mhs.unsyiah.ac.id>
2022-04-24 00:43:03 +0700

    edit home icon

--- resources/views/admin/layout/main.blade.php
+++ resources/views/admin/layout/main.blade.php
@@ -53,7 +53,7 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="index3.html" class="brand-link">
-                <img src="{{ url('assets/icon/Logo.png') }}" alt="Menheal Logo"
+                <img src="{{ url('assets/icon/favicon.ico') }}" alt="Menheal Logo"
                     class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">MenHeal</span>
             </a>
@@ -68,17 +68,8 @@
                         data-accordion="false">
 
                         <li class="nav-item">
-                            <a href="pages/widgets.html" class="nav-link">
-                                <i class="nav-icon fas fa-th"></i>
-                                <p>
-                                    Widgets
-                                    <span class="right badge badge-danger">New</span>
-                                </p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
                             <a href="{{ url('/admin') }}" class="nav-link">
-                                <i class="fas fa-home"></i>
+                                <i class="nav-icon fas fa-home"></i>
                                 <p>
                                     Home
                                 </p>
@@ -88,7 +79,7 @@
                         <!-- user login -->
                         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                             <div class="image">
-                                <img src="{{ url('assets/icon/Logo.png') }}" class="img-circle elevation-2"
+                                <img src="{{ url('assets/icon/favicon.ico') }}" class="img-circle elevation-2"
                                     alt="User Image">
                             </div>
                             <div class="info">
