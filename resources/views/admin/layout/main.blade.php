d93ef29c20212f999ef266da89026b174ac415e4
ikhsan <m_ikhsan20@mhs.unsyiah.ac.id>
2022-04-24 00:39:18 +0700

    add favicon in admin page

--- resources/views/admin/layout/main.blade.php
+++ resources/views/admin/layout/main.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Menheal | Dashboard</title>
 
+    <!-- icon page -->
+    <link rel="shortcut icon" href="{{ url('assets/icon/favicon.ico') }}" type="image/x-icon">
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
