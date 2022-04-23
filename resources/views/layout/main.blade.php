7cf675ed29d1a49fc39534f70b99f6b81b7059bf
Amar Suhendra <amarsuhendra2@gmail.com>
2022-04-23 12:08:57 +0700

    Add nav active and title

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -11,16 +11,16 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Menheal | home</title>
+    <title>Menheal | {{ $title }}</title>
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand ms-5 me-1" href="/">
+            <a class="navbar-brand ms-5 me-1">
                 <img src="assets/img/Icon.png" alt="Mengheal">
             </a>
-            <h3 class="company-name">Mengheal</h3>
+            <h3> <a href="{{ url('/home') }}"  class="company-name"> Mengheal</a> </h3>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
@@ -29,13 +29,13 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 ms-4 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link {{ ($title == "Home")? 'active fw-bold text-primary' : '' }} " aria-current="page" href="{{ url('/home') }}">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ url('/konsultasilangsung') }}">Konsultasi Langsung</a>
+                        <a class="nav-link {{ ($title == "Antrian")? 'active fw-bold text-primary' : '' }} " href="{{ url('/konsultasilangsung') }}" >Konsultasi Langsung</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ url('/konsultasionline') }}">Konsultasi Online</a>
+                        <a class="nav-link {{ ($title == "Konsultasi Online")? 'active fw-bold text-primary' : '' }} " href="{{ url('/konsultasionline') }}">Konsultasi Online</a>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
