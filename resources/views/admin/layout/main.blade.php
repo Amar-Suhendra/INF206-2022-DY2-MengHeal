33497c41963835de1bc467e60ff97143c5353f79
ikhsan <m_ikhsan20@mhs.unsyiah.ac.id>
2022-04-24 15:44:52 +0700

    fix brand logo link

--- resources/views/admin/layout/main.blade.php
+++ resources/views/admin/layout/main.blade.php
@@ -58,7 +58,7 @@
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
-            <a href="index3.html" class="brand-link">
+            <a href="{{ url('/admin') }}" class="brand-link">
                 <img src="{{ url('assets/icon/favicon.ico') }}" alt="Menheal Logo"
                     class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">MenHeal</span>
