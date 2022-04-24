7fa76cf41c371d2f9f71f6fba4e1a3c2fe19ffa9
ikhsan <m_ikhsan20@mhs.unsyiah.ac.id>
2022-04-24 21:07:12 +0700

    add data to registerseeder

--- database/seeders/registerSeeder.php
+++ database/seeders/registerSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class registerSeeder extends Seeder
 {
@@ -14,6 +15,13 @@ class registerSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('registers')->insert(
+            [
+                'name' => 'amar',
+                'username' => 'amar',
+                'password' => bcrypt('123'),
+            ]
+
+        );
     }
 }
