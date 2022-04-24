cea618d0feff0e04b74a25b13843630e89961d2d
ikhsan <m_ikhsan20@mhs.unsyiah.ac.id>
2022-04-24 21:13:21 +0700

    add status field at registers table

--- database/migrations/2022_04_21_091408_create_registers_table.php
+++ database/migrations/2022_04_21_091408_create_registers_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->string('Email')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('Password');
+            $table->boolean('status')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
