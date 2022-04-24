4b2098e33af50cc32688437e895442dd0aea667c
ikhsan <m_ikhsan20@mhs.unsyiah.ac.id>
2022-04-24 21:18:00 +0700

    add yield title page & title tab

--- resources/views/admin/layout/main.blade.php
+++ resources/views/admin/layout/main.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Menheal | Dashboard</title>
+    <title>Menheal | @yield('title-tab')</title>
 
     <!-- icon page -->
     <link rel="shortcut icon" href="{{ url('assets/icon/favicon.ico') }}" type="image/x-icon">
@@ -121,7 +121,7 @@
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6">
-                            <h1 class="m-0">Dashboard</h1>
+                            <h1 class="m-0">@yield('title-page')</h1>
                         </div><!-- /.col -->
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
