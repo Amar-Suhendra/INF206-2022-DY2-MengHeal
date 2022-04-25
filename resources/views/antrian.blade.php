de1cbe99296b9742ef04a964dd383b293727bc5d
ikhsan <m_ikhsan20@mhs.unsyiah.ac.id>
2022-04-25 10:58:26 +0700

    merubah sedikit tampilan ambil no antrian

--- resources/views/antrian.blade.php
+++ resources/views/antrian.blade.php
@@ -4,7 +4,11 @@
         <div class="card text-center " style="width: 18rem; height:16rem; border-radius: 10%">
             <div class="card-header bg-transparent">
                 <div class="card-title">
-                    <h3>No Antrian</h3>
+                    @if ($antrian->no_antrian)
+                        <h3>No Antrian Anda</h3>
+                    @else
+                        <h3>No Antrian</h3>
+                    @endif
                 </div>
             </div>
             <div class="card-body">
@@ -20,10 +24,12 @@
                 </div>
             </div>
             <div class="mb-2 d-grid gap-2 col-8 mx-auto bg-transparent">
-                <a href="{{ url('get-antrian/' . auth()->user()->id) }}" type="button"
-                    class="btn btn-primary text-white text-decoration-none fw-bold">
-                    Konfirmasi
-                </a>
+                @if (!$antrian->no_antrian)
+                    <a href="{{ url('get-antrian/' . auth()->user()->id) }}" type="button"
+                        class="btn btn-primary text-white text-decoration-none fw-bold">
+                        Ambil Antrian
+                    </a>
+                @endif
             </div>
         </div>
     </div>
