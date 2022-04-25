b78187c98ba8d1ebd754aff946a5538537908046
Amar Suhendra <amarsuhendra2@gmail.com>
2022-04-25 23:35:15 +0700

    Modified ChatController.php status: error

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Event\MessageSent;
+use App\Message;
+use Auth;
 
 class ChatController extends Controller
 {
@@ -10,5 +13,19 @@ class ChatController extends Controller
     public function index()
     {
         return view('konsul');
+    }
+
+    // get messages
+    public function getMessages()
+    {
+        // get all messages
+        // return as a resource
+        return Message::with('user')->get();;
+    }
+
+    // broadcast message
+    public function broadcastMessage(Request $request){
+        broadcast(new MessageSent($request->user, $request->message))->toOthers();
+        return response()->json(['status' => 'Message Sent!']);
     }
 }
