60614798bc163e4fc4bae48c02f8d6b0d9991b7d
Amar Suhendra <amarsuhendra2@gmail.com>
2022-04-25 16:21:53 +0700

    make model and controller

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -7,5 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-    use HasFactory;
+    protected $fillable = ['message']; //MENGIZINKAN MENYIMPAN DATA KE COLUMN MESSAGE
+
+    //RELASI KE TABLE USER UNTUK MENGAMBIL SIAPA PEMILIK PESAN TERSEBUT
+    public function user()
+    {
+        //KITA GUNAKAN BELONGSTO KARENA USER BERTINDAK SEBAGAI DATA INDUK
+        return $this->belongsTo(User::class);
+    }
 }
