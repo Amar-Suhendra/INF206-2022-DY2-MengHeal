81990e198b460901306ea8d5ddea9a973643d151
Muhammad Rizky Hidayah Akbar <mhidayahakbar@gmail.com>
2022-04-27 20:08:24 +0700

    masukkan link video

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.main')
 @section('container')
     <div class="d-flex justify-content-around py-2">
-        <div class="card text-center" style="width: 18rem; height:16rem; border-radius: 10%">
+        <div class="card text-center" style="width: 18rem; height:16rem; border-radius: 5%">
             <div class="card-header bg-transparent">
                 <div class="card-title">
                    <h5>video 1</h5>
@@ -11,14 +11,20 @@
                 <iframe src="https://www.youtube.com/embed/8iuLXODzL04" title="YouTube video" allowfullscreen></iframe>
             </div>
         </div>
-        <div class="card text-center " style="width: 18rem; height:16rem; border-radius: 10%">
+        <div class="card text-center " style="width: 18rem; height:16rem; border-radius: 5%">
             <div class="ratio ratio-16x9 mt-5">
                 <iframe src="https://www.youtube.com/embed/8iuLXODzL04" title="YouTube video" allowfullscreen></iframe>
             </div>
         </div>
-        <div class="card text-center " style="width: 18rem; height:16rem; border-radius: 10%">
-            <div class="ratio ratio-16x9">
+        <div class="card text-center " style="width: 18rem; height:16rem; border-radius: 5%">
+            <div class="ratio ratio-16x9 my-4">
                 <iframe src="https://www.youtube.com/embed/8iuLXODzL04" title="YouTube video" allowfullscreen></iframe>
+            </div>
+            <div class="mb-2 d-grid gap-2 col-8 mx-auto bg-transparent">
+                    <a href="" type="button"
+                        class="btn btn-primary text-white text-decoration-none fw-bold">
+                        nonton video
+                    </a>
             </div>
         </div>
     </div>
