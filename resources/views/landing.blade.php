926dad5bce0695054b2bd046035b75f7fbd246c0
Rudy Hidayat <m_rudy@mhs.unsyiah.ac.id>
2022-04-27 07:34:26 -0700

    Update Landing(2)

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -6,8 +6,8 @@
     <section class="hero-container">
         <div>
             <div>
-                <h1>Make better coffe</h1>
-                <p>why learn how to blog?</p>
+                <h1>Be better person</h1>
+                <p>why learn how to good people?</p>
             </div>
         </div>
         <img src="../assets/img/landing.jpg" width="600 px" height="300 px">
@@ -17,50 +17,64 @@
         <div class="kolom">
             <p class="deskripsi-1">Kami menyediakan</p>
             <h2>Curhat Online</h2>
-            <p>Halo, web ini dibuat untuk memenuhi tugas projek tim program Digital Talent Scholarship Progate.
-                Web ini ditujukan untuk memberikan informasi mengenai kursus, seminar dan workshop yang diselenggarakan 
-                oleh beberapa universitas.
+            <p>Halo sahabat, kalian dapat melakukan curhat dengan psikolog ahli yang kami miliki 
+                tanpa takut informasi kalian disalahgunakan. Kalian dapat memilih fitur chat dan curhatlah
+                kapanpun yang kalian inginkan. Semangat ya!
             </p>
         </div>
     </section>
     <section id="courses">
         <div class="kolom">
             <p class="deskripsi">Kami menyediakan</p>
-            <h2>Curhat Online</h2>
-            <p>Halo, web ini dibuat untuk memenuhi tugas projek tim program Digital Talent Scholarship Progate.
-                Web ini ditujukan untuk memberikan informasi mengenai kursus, seminar dan workshop yang diselenggarakan 
-                oleh beberapa universitas.
+            <h2>Quote Mantap Jiwa</h2>
+            <p>Halo guys, kalian lesu hari ini? Ayoo baca kata-kata luar biasa dari ahli-ahli kami.
+                Dijamin semangat hidup kalian bangkit lagi sampe rasanya ingin terbang. Kalian biasa
+                pilih fitur quote kami untuk melihat semua quote terbaik. Contoh nya dibawah ini nih!
             </p>
         </div>
         <img src="../assets/img/landing3.jpg" width="600px" height="300px">
     </section>
-    <section class="description-container">
+    <section class="quote-container">
         <div>
             <div>
-                <h2> long established</h2>
-                <p>It is a long established fact that a reader will be distracted by the readable content of a page when
-                    looking at its layout. The point of using Lorem Ipsum is that.</p>
+                <h2> Quote of the day</h2>
+                <p>"Jangan terlalu cepat menilai orang dan langsung menjadikan sahabat.
+                    Bagaikan gula dan garam, orang akan mengetahui bedanya setelah dirasa dan dicicipi. 
+                    Begitu juga manusia, anda akan dapat mengetahui siapa dia setelah mencoba bergaul dengannya.
+                </p>
             </div>
-            <div class="description-left-footer">
-                <p>May 20th 2020</p>
+            <div class="description-quote">
+                <p>Rudy Hidayat S.Kom,Lc,MA</p>
             </div>
         </div>
         <img src="../assets/img/landing4.jpg" alt="desc">
     </section>
-    <section class="what-is-container">
+    <section id="home">
+        <img src="../assets/img/landing5.jpg" width="600px" height="300px">
+        <div class="kolom">
+            <p class="deskripsi-1">Kami menyediakan</p>
+            <h2>VIdeo Booster</h2>
+            <p>Halo sahabat syurga, kalian ingin dapat motivasi tapi mager membaca? Web kami menyediakan
+                fitur video booster lhoo. Kalian dapat nonton video-video terbaik yang akan meningkatkan
+                semangat hari senin kalian!
+            </p>
+        </div>
+    </section>
+    <section class="last-container">
         <div>
             <div>
-                <h1>What is Lorem Ipsum?</h1>
-                <p>It is a long established fact that a reader will be distracted by the readable content of a page when
-                    looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal
-                    distribution...</p>
+                <h1>Apa itu website MengHeal?</h1>
+                <p>MengHeal adalah website yang berfokus menyemangati hari-hari kalian.
+                    Kami tidak mau mental kalian menjadi down hanya karena tugas, praktik, dan lain-lain.
+                    Maka kami membuat website ini agar kalian dapat curhat, kalian dapat membaca ataupun
+                    nonton video yang akan menyemangati kehidupan kalian!
+                </p>
             </div>
             <div class="footer">
-                <p>May 20</p>
-                <p class="font-weight-bold">Read more</p>
+                <p>MengHeal Foundation</p>
             </div>
         </div>
-        <img src="../assets/img/landing5.jpg" alt="what-is">
+        <img src="../assets/img/landing6.jpg" alt="last">
     </section>
 
 </div>
