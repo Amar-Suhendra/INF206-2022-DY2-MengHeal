b59f1037699a7c8a6b8bae7110b4075abea66a8b
Muhammad Rizky Hidayah Akbar <mhidayahakbar@gmail.com>
2022-04-28 10:41:47 +0700

    menambahin video

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -12,29 +12,57 @@
             </div>
         </div>
         <div class="card text-center " style="width: 18rem; height:16rem; border-radius: 5%">
-            <div class="ratio ratio-16x9 mt-5">
+            <div class="card-header bg-transparent">
+                <div class="card-title">
+                   <h5>video 2</h5>
+                </div>
+            </div>
+            <div class="ratio ratio-16x9 mt-2">
                 <iframe src="https://www.youtube.com/embed/8iuLXODzL04" title="YouTube video" allowfullscreen></iframe>
             </div>
         </div>
         <div class="card text-center " style="width: 18rem; height:16rem; border-radius: 5%">
-            <div class="ratio ratio-16x9 my-4">
-                <iframe src="https://www.youtube.com/embed/8iuLXODzL04" title="YouTube video" allowfullscreen></iframe>
+            <div class="card-header bg-transparent">
+                <div class="card-title">
+                   <h5>video 3</h5>
+                </div>
             </div>
-            <div class="mb-2 d-grid gap-2 col-8 mx-auto bg-transparent">
-                    <a href="" type="button"
-                        class="btn btn-primary text-white text-decoration-none fw-bold">
-                        nonton video
-                    </a>
+            <div class="ratio ratio-16x9 mt-2">
+                <iframe src="https://www.youtube.com/embed/8iuLXODzL04" title="YouTube video" allowfullscreen></iframe>
             </div>
         </div>
     </div>
     
     <div class="d-flex justify-content-around py-5">
         <div class="card text-center" style="width: 18rem; height:16rem; border-radius: 10%">
+            <div class="card-header bg-transparent">
+                <div class="card-title">
+                   <h5>video 4</h5>
+                </div>
+            </div>
+            <div class="ratio ratio-16x9 mt-2">
+                <iframe src="https://www.youtube.com/embed/8iuLXODzL04" title="YouTube video" allowfullscreen></iframe>
+            </div>
         </div>
         <div class="card text-center " style="width: 18rem; height:16rem; border-radius: 10%">
+            <div class="card-header bg-transparent">
+                <div class="card-title">
+                   <h5>video 5</h5>
+                </div>
+            </div>
+            <div class="ratio ratio-16x9 mt-2">
+                <iframe src="https://www.youtube.com/embed/8iuLXODzL04" title="YouTube video" allowfullscreen></iframe>
+            </div>
         </div>
         <div class="card text-center " style="width: 18rem; height:16rem; border-radius: 10%">
+            <div class="card-header bg-transparent">
+                <div class="card-title">
+                   <h5>video 6</h5>
+                </div>
+            </div>
+            <div class="ratio ratio-16x9 mt-2">
+                <iframe src="https://www.youtube.com/embed/8iuLXODzL04" title="YouTube video" allowfullscreen></iframe>
+            </div>
         </div>
     </div>
 @endsection
