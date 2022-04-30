c3270424ad4ae160f829af9c9bcfcf1c5927cf88
Amar Suhendra <amarsuhendra2@gmail.com>
2022-05-01 05:53:04 +0700

    modified Event Message.php

--- app/Events/Message.php
+++ app/Events/Message.php
@@ -10,18 +10,23 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class Message
+class Message implements shouldBroadcast // Delete implements shouldBroadcast for default code
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    // variables for chat
+    public $username;
+    public $message;
 
     /**
      * Create a new event instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($username, $message)
     {
-        //
+        $this->username = $username;
+        $this->message = $message;
     }
 
     /**
@@ -29,8 +34,23 @@ class Message
      *
      * @return \Illuminate\Broadcasting\Channel|array
      */
+
+    /** OLD CODE
+     * / public function broadcastOn()
+     * {
+     *     return new PrivateChannel('channel-name');
+     * }
+     */
+
+     // NEW CODE
     public function broadcastOn()
     {
-        return new PrivateChannel('channel-name');
+        return new Channel('chat');
+    }
+
+    // Alias for broadcastOn
+    public function broadcastAs()
+    {
+        return 'message';
     }
 }
