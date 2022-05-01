388196a9a99e0dee90236c332fe4619c55631f62
Amar Suhendra <amarsuhendra2@gmail.com>
2022-05-01 16:51:30 +0700

    change title

--- resources/views/admin/layout/dashboard.blade.php
+++ resources/views/admin/layout/dashboard.blade.php
@@ -19,7 +19,7 @@
                 <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-users"></i></span>
 
                 <div class="info-box-content">
-                    <span class="info-box-text">New Members</span>
+                    <span class="info-box-text">Registered Member</span>
                     <span class="info-box-number">{{ $count }}</span>
                 </div>
                 <!-- /.info-box-content -->
