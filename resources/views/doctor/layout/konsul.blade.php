818cb6c634e69b0d766fdd86cea163af417d593a
Amar Suhendra <amarsuhendra2@gmail.com>
2022-05-04 23:08:29 +0700

    onliine consultation for doctor done

--- resources/views/doctor/layout/konsul.blade.php
+++ resources/views/doctor/layout/konsul.blade.php
@@ -4,21 +4,19 @@
 @section('title-page', 'Online Consultation')
 
 @section('mainContent')
+    <link rel="stylesheet" href="../css/app.css">
     <!-- Info boxes -->
-    <div class="row">
-
-
-
-        <!-- fix for small devices only -->
-        <div class="clearfix hidden-md-up"></div>
-
-
-        <!-- /.col -->
-        <div class="col-12 col-sm-6 col-md-3">
-            <h1>Online Consultation</h1>
-            <!-- /.info-box -->
-        </div>
-        <!-- /.col -->
+    <div class="app">
+        <header class="chat-header">
+            <h1 class="chat-judul">Konsultasi Online</h1>
+            <input class="form-control" type="text" name="username" value="{{ auth()->user()->name }}" id="username" disabled>
+        </header>
+        <div id="messages"></div>
+        <form id="message_form">
+            <input class="form-control" type="text" name="message_input" id="message_input" placeholder="Enter Message">
+            <button class="btn btn-primary" type="submit" id="message_send">Send<ion-icon name="send-outline">
+                </ion-icon></button>
+        </form>
     </div>
-    <!-- /.row -->
+    <script src="../js/app.js"></script>
 @endsection
