0b8668ce8f7f4397603b024b1e7bbcdfefb6cb35
Muhammad Rizky Hidayah Akbar <mhidayahakbar@gmail.com>
2022-05-05 16:23:03 +0700

    tambahan video 2

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -7,8 +7,10 @@
                    <h6>MOTIVASI HIDUP & NASIHAT PALING MENYENTUH YANG AKAN MENGUBAH NASIBMU</h6>
                 </div>
             </div>
-            <div class="ratio ratio-16x9 mt-2">
-                <iframe src="https://www.youtube.com/embed/SCfxVgtcDVs" title="YouTube video" allowfullscreen></iframe>
+            <div class="card-body">
+                <div class="ratio ratio-16x9 mt-2">
+                    <iframe src="https://www.youtube.com/embed/SCfxVgtcDVs" title="YouTube video" allowfullscreen></iframe>
+                </div>
             </div>
         </div>
         <div class="card text-center " style="width: 21rem; height:19rem; border-radius: 10%">
@@ -17,18 +19,22 @@
                    <h6>SATU KATA AJAIB MENGUBAH HIDUPMU</h6>
                 </div>
             </div>
-            <div class="ratio ratio-16x9 mt-2">
-                <iframe src="https://www.youtube.com/embed/8iuLXODzL04" title="YouTube video" allowfullscreen></iframe>
-            </div>
-        </div>
-        <div class="card text-center " style="width: 19rem; height:17rem; border-radius: 10%">
-            <div class="card-header bg-transparent">
-                <div class="card-title">
-                   <h5>video 3</h5>
+            <div class="card-body">
+                <div class="ratio ratio-16x9 mt-2">
+                    <iframe src="https://www.youtube.com/embed/92L2H23TEyg" title="YouTube video" allowfullscreen></iframe>
                 </div>
             </div>
-            <div class="ratio ratio-16x9 mt-2">
-                <iframe src="https://www.youtube.com/embed/8iuLXODzL04" title="YouTube video" allowfullscreen></iframe>
+        </div>
+        <div class="card text-center " style="width: 21rem; height:19rem; border-radius: 10%">
+            <div class="card-header bg-transparent">
+                <div class="card-title">
+                   <h6>Motivasi Hidup, Kata Kata Penyemangat Diri di Masa yang Sulit</h6>
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="ratio ratio-16x9 mt-2">
+                    <iframe src="https://www.youtube.com/embed/8iuLXODzL04" title="YouTube video" allowfullscreen></iframe>
+                </div>
             </div>
         </div>
     </div>
