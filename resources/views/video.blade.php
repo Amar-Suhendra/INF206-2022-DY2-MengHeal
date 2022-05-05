07db8644e780665964c613770c7a7347f0cb46bd
Muhammad Rizky Hidayah Akbar <mhidayahakbar@gmail.com>
2022-05-05 16:14:02 +0700

    tambahan 1 video

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -1,27 +1,27 @@
 @extends('layout.main')
 @section('container')
     <div class="d-flex justify-content-around py-2">
-        <div class="card text-center" style="width: 18rem; height:16rem; border-radius: 5%">
+        <div class="card text-center" style="width: 21rem; height:19rem; border-radius: 10%">
             <div class="card-header bg-transparent">
                 <div class="card-title">
-                   <h5>video 1</h5>
+                   <h6>MOTIVASI HIDUP & NASIHAT PALING MENYENTUH YANG AKAN MENGUBAH NASIBMU</h6>
+                </div>
+            </div>
+            <div class="ratio ratio-16x9 mt-2">
+                <iframe src="https://www.youtube.com/embed/SCfxVgtcDVs" title="YouTube video" allowfullscreen></iframe>
+            </div>
+        </div>
+        <div class="card text-center " style="width: 21rem; height:19rem; border-radius: 10%">
+            <div class="card-header bg-transparent">
+                <div class="card-title">
+                   <h6>SATU KATA AJAIB MENGUBAH HIDUPMU</h6>
                 </div>
             </div>
             <div class="ratio ratio-16x9 mt-2">
                 <iframe src="https://www.youtube.com/embed/8iuLXODzL04" title="YouTube video" allowfullscreen></iframe>
             </div>
         </div>
-        <div class="card text-center " style="width: 18rem; height:16rem; border-radius: 5%">
-            <div class="card-header bg-transparent">
-                <div class="card-title">
-                   <h5>video 2</h5>
-                </div>
-            </div>
-            <div class="ratio ratio-16x9 mt-2">
-                <iframe src="https://www.youtube.com/embed/8iuLXODzL04" title="YouTube video" allowfullscreen></iframe>
-            </div>
-        </div>
-        <div class="card text-center " style="width: 18rem; height:16rem; border-radius: 5%">
+        <div class="card text-center " style="width: 19rem; height:17rem; border-radius: 10%">
             <div class="card-header bg-transparent">
                 <div class="card-title">
                    <h5>video 3</h5>
@@ -34,7 +34,7 @@
     </div>
     
     <div class="d-flex justify-content-around py-5">
-        <div class="card text-center" style="width: 18rem; height:16rem; border-radius: 10%">
+        <div class="card text-center" style="width: 19rem; height:17rem; border-radius: 10%">
             <div class="card-header bg-transparent">
                 <div class="card-title">
                    <h5>video 4</h5>
@@ -44,7 +44,7 @@
                 <iframe src="https://www.youtube.com/embed/8iuLXODzL04" title="YouTube video" allowfullscreen></iframe>
             </div>
         </div>
-        <div class="card text-center " style="width: 18rem; height:16rem; border-radius: 10%">
+        <div class="card text-center " style="width: 19rem; height:17rem; border-radius: 10%">
             <div class="card-header bg-transparent">
                 <div class="card-title">
                    <h5>video 5</h5>
@@ -54,7 +54,7 @@
                 <iframe src="https://www.youtube.com/embed/8iuLXODzL04" title="YouTube video" allowfullscreen></iframe>
             </div>
         </div>
-        <div class="card text-center " style="width: 18rem; height:16rem; border-radius: 10%">
+        <div class="card text-center " style="width: 19rem; height:17rem; border-radius: 10%">
             <div class="card-header bg-transparent">
                 <div class="card-title">
                    <h5>video 6</h5>
