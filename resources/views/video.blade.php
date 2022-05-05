b7ce907e3be3dfa1feb36fc007d8a7e5a15ec66b
Muhammad Rizky Hidayah Akbar <mhidayahakbar@gmail.com>
2022-05-05 16:34:48 +0700

    tambahan video 4

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -40,14 +40,16 @@
     </div>
     
     <div class="d-flex justify-content-around py-5">
-        <div class="card text-center" style="width: 19rem; height:17rem; border-radius: 10%">
+        <div class="card text-center" style="width: 21rem; height:19rem; border-radius: 10%">
             <div class="card-header bg-transparent">
                 <div class="card-title">
-                   <h5>video 4</h5>
+                   <h6>Video Motivasi Untuk Membangkitkan Semangat Hidup</h6>
                 </div>
             </div>
-            <div class="ratio ratio-16x9 mt-2">
-                <iframe src="https://www.youtube.com/embed/8iuLXODzL04" title="YouTube video" allowfullscreen></iframe>
+            <div class="card-body">
+                <div class="ratio ratio-16x9 mt-2">
+                    <iframe src="https://www.youtube.com/embed/GobUO6Jwt7A" title="YouTube video" allowfullscreen></iframe>
+                </div>
             </div>
         </div>
         <div class="card text-center " style="width: 19rem; height:17rem; border-radius: 10%">
