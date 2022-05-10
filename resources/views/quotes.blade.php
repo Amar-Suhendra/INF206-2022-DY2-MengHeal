4435e11682dd09159089c789b74f9d96cf171793
Amar Suhendra <amarsuhendra2@gmail.com>
2022-05-10 12:40:43 +0700

    fix quotes styling

--- resources/views/quotes.blade.php
+++ resources/views/quotes.blade.php
@@ -1,51 +1,22 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <title>Quotes</title>
-  </head>
-
-  <style type="text/css">
-    .list-group, .list-group-item {
-        font-family: sans-serif; 
-        font-size: 14px; 
-        font-weight: 600;
-        font-style: oblique;
-    }
-
-    .gambar {
-        margin: 30px;
-    }
-
-    .container {
-        margin: 30px;
-    }
-</style>
-
-  <body>
+@extends('layout.main')
+@section('container')
     <div class="container">
-        <h2>Quotes of the day</h2>
-        <div class="list-group">
+        {{-- <h2>Quotes of the day</h2> --}}
+        <div class="card shadow border-primary w-50 p-3">
+            <p class="fw-bold fst-italic">
+                "Health is a state of complete physical, mental, and social well-
+                being and not merely the absence of disease or infirmity."
+            </p>
+            <p class="text-end">
+                -World Health Oragnization (WHO)
+            </p>
+        </div>
+
+        {{-- <div class="list-group">
+            
             <div class="list-group-item">
-                <img class="gambar" src="C:\xampp\htdocs\INF206-2022-DY2-MengHeal\resources\views\q1.png" width="150" height="150">
                 <p>
-                    "Health is a state of complete physical, mental, and social well-
-                    being and not merely the absence of disease or infirmity."
-                </p>
-                <p>
-                    -World Health Oragnization (WHO)
-                </p>
-            </div>
-            <div class="list-group-item">
-                <img class="gambar" src="C:\xampp\htdocs\INF206-2022-DY2-MengHeal\resources\views\q2.png" width="150" height="150">
-                <p>
-                    "Your self-worth is determined by you. 
+                    "Your self-worth is determined by you.
                     You don’t have to depend on someone telling you who you are.”
                 </p>
                 <p>
@@ -53,9 +24,8 @@
                 </p>
             </div>
             <div class="list-group-item">
-                <img class="gambar" src="C:\xampp\htdocs\INF206-2022-DY2-MengHeal\resources\views\q3.png" width="150" height="150">
                 <p>
-                    "You don’t see the mental ilness: It’s not a mass, it’s not a cyst. 
+                    "You don’t see the mental ilness: It’s not a mass, it’s not a cyst.
                     But it’s there. Why do you need to prove it!”
                 </p>
                 <p>
@@ -63,9 +33,9 @@
                 </p>
             </div>
             <div class="list-group-item">
-                <img class="gambar" src="C:\xampp\htdocs\INF206-2022-DY2-MengHeal\resources\views\q4.png" width="150" height="150">
                 <p>
-                    "I found that with depression, one of the most important things you could realize is that you’re not alone. 
+                    "I found that with depression, one of the most important things you could realize is that you’re not
+                    alone.
                     You’re not the first to go through it; you’re not going to be the last to go through it."
                 </p>
                 <p>
@@ -73,26 +43,14 @@
                 </p>
             </div>
             <div class="list-group-item">
-                <img class="gambar" src="C:\xampp\htdocs\INF206-2022-DY2-MengHeal\resources\views\q5.png" width="150" height="150">
                 <p>
-                    "I finally realized that owning up to your vulnerabilities is a form of strength. 
+                    "I finally realized that owning up to your vulnerabilities is a form of strength.
                     And making the choice to go to therapy is a form of strength."
                 </p>
                 <p>
                     -Lizzo
                 </p>
             </div>
-        </div>
+        </div> --}}
     </div>
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-    -->
-  </body>
-</html>
+@endsection
