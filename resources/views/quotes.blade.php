bbae6e638403a2013c6b99a15f8a4acf5667c5ed
Amar Suhendra <amarsuhendra2@gmail.com>
2022-05-10 14:28:09 +0700

    Fix another quotes styling

--- resources/views/quotes.blade.php
+++ resources/views/quotes.blade.php
@@ -2,7 +2,7 @@
 @section('container')
     <div class="container">
         {{-- <h2>Quotes of the day</h2> --}}
-        <div class="card shadow border-primary w-50 p-3">
+        <div class="card shadow border-primary w-50 p-3 mb-5">
             <p class="fw-bold fst-italic">
                 "Health is a state of complete physical, mental, and social well-
                 being and not merely the absence of disease or infirmity."
@@ -11,46 +11,42 @@
                 -World Health Oragnization (WHO)
             </p>
         </div>
-
-        {{-- <div class="list-group">
-            
-            <div class="list-group-item">
-                <p>
-                    "Your self-worth is determined by you.
-                    You don’t have to depend on someone telling you who you are.”
-                </p>
-                <p>
-                    -Beyoncé Knowles-Carter
-                </p>
-            </div>
-            <div class="list-group-item">
-                <p>
-                    "You don’t see the mental ilness: It’s not a mass, it’s not a cyst.
-                    But it’s there. Why do you need to prove it!”
-                </p>
-                <p>
-                    -Amanda Seyfried
-                </p>
-            </div>
-            <div class="list-group-item">
-                <p>
-                    "I found that with depression, one of the most important things you could realize is that you’re not
-                    alone.
-                    You’re not the first to go through it; you’re not going to be the last to go through it."
-                </p>
-                <p>
-                    -The Rock
-                </p>
-            </div>
-            <div class="list-group-item">
-                <p>
-                    "I finally realized that owning up to your vulnerabilities is a form of strength.
-                    And making the choice to go to therapy is a form of strength."
-                </p>
-                <p>
-                    -Lizzo
-                </p>
-            </div>
-        </div> --}}
+        <div class="card shadow border-primary w-50 p-3 mb-5">
+            <p class="fw-bold fst-italic">
+                "Your self-worth is determined by you.
+                You don’t have to depend on someone telling you who you are.”
+            </p>
+            <p class="text-end">
+                -Beyoncé Knowles-Carter
+            </p>
+        </div>
+        <div class="card shadow border-primary w-50 p-3 mb-5">
+            <p class="fw-bold fst-italic">
+                "You don’t see the mental ilness: It’s not a mass, it’s not a cyst.
+                But it’s there. Why do you need to prove it!”
+            </p>
+            <p class="text-end">
+                -Amanda Seyfried
+            </p>
+        </div>
+        <div class="card shadow border-primary w-50 p-3 mb-5">
+            <p class="fw-bold fst-italic">
+                "I found that with depression, one of the most important things you could realize is that you’re not
+                alone.
+                You’re not the first to go through it; you’re not going to be the last to go through it."
+            </p>
+            <p class="text-end">
+                -The Rock
+            </p>
+        </div>
+        <div class="card shadow border-primary w-50 p-3 mb-5">
+            <p class="fw-bold fst-italic">
+                "I finally realized that owning up to your vulnerabilities is a form of strength.
+                And making the choice to go to therapy is a form of strength."
+            </p>
+            <p class="text-end">
+                -Lizzo
+            </p>
+        </div>
     </div>
 @endsection
