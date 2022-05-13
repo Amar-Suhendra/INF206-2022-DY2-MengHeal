49d3c47e225074c5284d9cd2db39b7472e625753
ikhsan <m_ikhsan20@mhs.unsyiah.ac.id>
2022-05-13 21:51:48 +0700

    fix image error

--- resources/views/admin/layout/main.blade.php
+++ resources/views/admin/layout/main.blade.php
@@ -134,7 +134,7 @@
                         <!-- user login -->
                         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                             <div class="image">
-                                <img src="{{ url('assets/icon/favicon.ico') }}" class="img-circle elevation-2"
+                                <img src="{{ url('assets/icon/icon.ico') }}" class="img-circle elevation-2"
                                     alt="User Image">
                             </div>
                             <div class="info">
