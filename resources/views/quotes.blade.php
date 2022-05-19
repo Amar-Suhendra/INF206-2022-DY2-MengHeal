4bc08fe26a8fa020c2ac8ac888f1cbe519a7ebbd
Amar Suhendra <amarsuhendra2@gmail.com>
2022-05-19 21:18:55 +0700

    change margin bottom

--- resources/views/quotes.blade.php
+++ resources/views/quotes.blade.php
@@ -1,22 +1,14 @@
 @extends('layout.main')
 @section('container')
     <div class="container">
-        <h2 class="mb-5">Quotes of the day</h2>
+        <h2 class="mb-3">Quotes of the day</h2>
         <div class="d-flex">
-            {{-- <div class="card shadow border-primary w-50 p-3 mb-5 me-5">
-                <p class="fw-bold fst-italic">
-                    "{{ $quotes->quotes }}"
-                </p>
-                <p class="text-end">
-                    -{{ $quotes->author }}
-                </p>
-            </div> --}}
             @php
                 $i = 1;
             @endphp
             <div class="row">
                 @foreach ($quote as $quotes)
-                    <div class="col-sm bg-white shadow p-3 border  mb-5 me-5">
+                    <div class="col-sm bg-white shadow p-3 border  mb-3 me-3">
                         <div class="small-box">
                             <div class="inner">
                                 <p class="fw-bold fst-italic">
