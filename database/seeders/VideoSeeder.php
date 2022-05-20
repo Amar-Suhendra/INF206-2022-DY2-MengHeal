8c9bba76266525ea4956df8f0c14c7f0743b5f1e
Ainal Fajri Malahayati <ainalfajrim@gmail.com>
2022-05-20 22:50:40 +0700

    video seeder

---
+++ database/seeders/VideoSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class VideoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('videos')->insert
+        (
+            [
+                'judul_vid' => ' ',
+                'url' => ' ',
+            ]
+        );
+    }
+}
